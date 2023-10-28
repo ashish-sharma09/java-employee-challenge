9405787d239553bde5c6c9c2cd7daf9904a18e26
Ashish Sharma <ashish.sharma@iconsolutions.com>
2023-10-28 12:49:58 +0100

    Initial Test setup with Wiremock, RestAssured and springboot

--- src/test/java/com/example/rqchallenge/RqChallengeApplicationTests.java
+++ src/test/java/com/example/rqchallenge/RqChallengeApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.rqchallenge;
 
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+
+@ExtendWith(SpringExtension.class) // TODO check if we need this
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RqChallengeApplicationTests {
 
+    @RegisterExtension
+    static WireMockExtension wireMockServer = WireMockExtension.newInstance()
+            .options(wireMockConfig().dynamicPort())
+            .build();
+
+    @LocalServerPort
+    private int port;
+
     @Test
-    void contextLoads() {
+    void contextLoads() { // TODO remove this later
     }
+
+
 
 }
