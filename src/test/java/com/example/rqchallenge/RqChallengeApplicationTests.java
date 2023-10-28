288f217775d181242d256d8f6f79bf9601f54016
Ashish Sharma <ashish.sharma@iconsolutions.com>
2023-10-28 14:31:34 +0100

    Test setup with communication over HTTPS

--- src/test/java/com/example/rqchallenge/RqChallengeApplicationTests.java
+++ src/test/java/com/example/rqchallenge/RqChallengeApplicationTests.java
@@ -1,6 +1,11 @@
 package com.example.rqchallenge;
 
+import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import io.restassured.RestAssured;
+import lombok.SneakyThrows;
+import net.bytebuddy.pool.TypePool;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.RegisterExtension;
@@ -8,7 +13,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.net.URISyntaxException;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static io.restassured.RestAssured.given;
 
 @ExtendWith(SpringExtension.class) // TODO check if we need this
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -16,7 +25,11 @@ class RqChallengeApplicationTests {
 
     @RegisterExtension
     static WireMockExtension wireMockServer = WireMockExtension.newInstance()
-            .options(wireMockConfig().dynamicPort())
+            .options(wireMockConfig()
+                    .dynamicHttpsPort()
+                    .keystorePath(resourcePath("dummyservice.jks"))
+                    .keystorePassword("changeit")
+                    .keyManagerPassword("changeit"))
             .build();
 
     @LocalServerPort
@@ -26,6 +39,34 @@ class RqChallengeApplicationTests {
     void contextLoads() { // TODO remove this later
     }
 
+    @Test
+    void checkDummyServiceTLSConfiguration() {
 
+        String status = "        {" +
+                "            \"status\": \"success\"" +
+                "        }";
+
+        wireMockServer.stubFor(
+                WireMock.get("/status")
+                        .willReturn(aResponse().withBody(status))
+        );
+
+        RestAssured.baseURI = "https://localhost:" + wireMockServer.getHttpsPort();
+
+        given()
+                .trustStore(resourcePath("dummyservice.jks"), "changeit")
+                .when()
+                .get("/status")
+                .then()
+                .body(Matchers.is(status));
+    }
+
+    private static String resourcePath(String resourceName) {
+        try {
+            return RqChallengeApplicationTests.class.getClassLoader().getResource(resourceName).toURI().getPath();
+        } catch (URISyntaxException e) {
+            throw new IllegalStateException("Could not find given resource: " + resourceName);
+        }
+    }
 
 }
